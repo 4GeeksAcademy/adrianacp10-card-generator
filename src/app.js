fac5a340127c262ecb4e178536fcd8fff7e79b1d
adrianacp10 <144694058+adrianacp10@users.noreply.github.com>
2024-01-03 16:38:08 +0000

    final

--- src/app.js
+++ src/app.js
@@ -6,6 +6,21 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  let numbers = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"];
+  let icon = ["♦", "♥", "♠", "♣"];
+
+  let RandomNumbers = numbers[Math.floor(Math.random() * numbers.length)];
+  let RandomIcon = icon[Math.floor(Math.random() * icon.length)];
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".top-icon").innerHTML = RandomIcon;
+  document.querySelector(".number").innerHTML = RandomNumbers;
+  document.querySelector(".bottom-icon").innerHTML = RandomIcon;
+
+  if (RandomIcon == "♦" || RandomIcon == "♥") {
+    document.querySelector(".top-icon").classList.add("red");
+    document.querySelector(".bottom-icon").classList.add("red");
+  } else {
+    document.querySelector(".bottom-icon").classList.add("black");
+    document.querySelector(".top-icon").classList.add("black");
+  }
 };
